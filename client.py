d9af9c6ff9c932521429ea1fd11a6b8be451f36f
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-10 18:55:41 +0530

    Basic Client server made

---
+++ client.py
@@ -0,0 +1,6 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+msg = s.recv(1024)
---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
