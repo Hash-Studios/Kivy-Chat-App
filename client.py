3dd7f35edadd88704069a2e20e01cfc82f890298
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-14 17:13:09 +0530

    Added buffering and streaming data

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 import socket
 
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+full_msg = ""
+new_msg = True
+while True:
+    msg = s.recv(HEADER_SIZE)
+    if new_msg:
+        print(f"New message length : {msg[:HEADER_SIZE]}")
+        msg_len = int(msg[:HEADER_SIZE])
+        new_msg = False
+
+    full_msg += msg.decode("utf-8")
+
+    if len(full_msg)-HEADER_SIZE == msg_len:
+        print("Full message received")
+        print(full_msg[HEADER_SIZE:])
+        new_msg = True
+        full_msg = ""
+print(full_msg)
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import socket
+import time
+import random
+
+HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -6,5 +10,16 @@ s.listen(5)
 
 while True:
     clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    print(f"Conentiion from {address} has been established!")
+
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADER_SIZE}}{msg}"
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(2)
+        msg = str(random.randint(0,999999))
+        msg = f"{len(msg):<{HEADER_SIZE}}{msg}"
+
+        clientsocket.send(bytes(msg, "utf-8"))
