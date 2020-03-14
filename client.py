c1f3532e6d15fa1b5b14ed932d8ded4d371afce6
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-14 17:28:49 +0530

    Used pickle to buffer python objects

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import socket
+import pickle
 
 HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ""
+full_msg = b""
 new_msg = True
 while True:
     msg = s.recv(HEADER_SIZE)
@@ -14,11 +15,14 @@ while True:
         msg_len = int(msg[:HEADER_SIZE])
         new_msg = False
 
-    full_msg += msg.decode("utf-8")
+    full_msg += msg
 
     if len(full_msg)-HEADER_SIZE == msg_len:
         print("Full message received")
-        print(full_msg[HEADER_SIZE:])
+        # print(full_msg[HEADER_SIZE:])
+
+        data = pickle.loads(full_msg[HEADER_SIZE:])
+        print(data[1]+data[2])
         new_msg = True
-        full_msg = ""
+        full_msg = b""
 print(full_msg)
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import time
 import random
+import pickle
+
 
 HEADER_SIZE = 10
 
@@ -12,14 +14,8 @@ while True:
     clientsocket, address = s.accept()
     print(f"Conentiion from {address} has been established!")
 
-    msg = "Welcome to the server!"
-    msg = f"{len(msg):<{HEADER_SIZE}}{msg}"
+    data = {1: "Hello,", 2: " World!"}
+    msg = pickle.dumps(data)
+    msg = bytes(f"{len(msg):<{HEADER_SIZE}}", "utf-8") + msg
 
-    clientsocket.send(bytes(msg, "utf-8"))
-
-    while True:
-        time.sleep(2)
-        msg = str(random.randint(0,999999))
-        msg = f"{len(msg):<{HEADER_SIZE}}{msg}"
-
-        clientsocket.send(bytes(msg, "utf-8"))
+    clientsocket.send(msg)
