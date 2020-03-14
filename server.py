a8f658039d7f99b07de178291631446317bf2614
Abhay Maurya <maurya.abhay30@gmail.com>
2020-03-14 18:49:31 +0530

    Updated server to broadcast messages

--- server.py
+++ server.py
@@ -1,21 +1,71 @@
 import socket
-import time
-import random
-import pickle
-
+import select
 
 HEADER_SIZE = 10
+IP = "127.0.0.1"
+PORT = 1234
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+
+def receive_msg(client_socket):
+    try:
+        msg_header = client_socket.recv(HEADER_SIZE)
+
+        if not len(msg_header):
+            return False
+
+        msg_len = int(msg_header.decode("utf-8").strip())
+        return {"header": msg_header, "data": client_socket.recv(msg_len)}
+
+    except:
+        return False
+
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Conentiion from {address} has been established!")
+    read_sockets, _, execption_sockets = select.select(
+        sockets_list, [], sockets_list)
 
-    data = {1: "Hello,", 2: " World!"}
-    msg = pickle.dumps(data)
-    msg = bytes(f"{len(msg):<{HEADER_SIZE}}", "utf-8") + msg
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
 
-    clientsocket.send(msg)
+            user = receive_msg(client_socket)
+            if not user:
+                continue
+            else:
+                sockets_list.append(client_socket)
+                clients[client_socket] = user
+                data = user['data'].decode("utf-8")
+                print(
+                    f"Accepted new connection from {client_address[0]}:{client_address[1]} with data {data}")
+
+        else:
+            msg = receive_msg(notified_socket)
+
+            if not msg:
+                client_name = clients[notified_socket]['data'].decode("utf-8")
+                print(f"Connection closed from {client_name}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            user_name = user['data'].decode("utf-8")
+            msg_data = msg["data"].decode("utf-8")
+            print(f"Received message from {user_name} : {msg_data}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(
+                        user['header'] + user['data'] + msg['header'] + msg['data'])
+    for notified_socket in execption_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
